72a45f14f0920e783e43bda532815279ea1ecfae
Francesco Castronuovo <118483640+francesco-castronuovo@users.noreply.github.com>
2023-02-16 15:10:03 +0100

    Create a copy of collection list wrapper
    Added a functionality to programmatically create a clone of the collection list wrapper, in order to avoid the need to do it with Webflow designer.

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
+const wrapper = document.querySelector('[fc-marquee=wrapper]')
+const wrapperClone = wrapper.cloneNode(true);
+
+// Update the ID and add a class
+//clone.id = 'elem2';
+//clone.classList.add('text-large');
+
+wrapperClone.after(wrapper)
+
 let duration = document.querySelector('[fc-marquee-duration]').getAttribute('fc-marquee-duration')
 let wrappers = document.querySelectorAll('[fc-marquee=wrapper]')
+
 for(const wrapper of wrappers)
 {
   wrapper.style.animationDuration = `${duration}s`
